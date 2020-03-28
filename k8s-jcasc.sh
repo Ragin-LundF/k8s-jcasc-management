6e6b14a5d445c8c0ac088d9bb48dd2fffaa921ad
Ragin <ragin666@gmail.com>
2020-03-28 16:57:11 +0100

    Adding main script and helper script to export all kubernetes configurations

---
+++ k8s-jcasc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# read configuration
+source ./config/k8s_jcasc_mgmt.cnf
+
+# import subscripts
+source ./scripts/arguments.sh
+source scripts/ipconfig_utils.sh
+source ./scripts/secrets_controller.sh
+
+# start the script
+processArguments $@
+
