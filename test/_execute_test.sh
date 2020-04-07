d8a5927dfc22e91c3f9190ca9deab4236c4504aa
Ragin <ragin666@gmail.com>
2020-04-07 12:26:47 +0200

    Adding more tests

--- test/_execute_test.sh
+++ test/_execute_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Starting ipconfig_utils test..."
+./ipconfig_utils_test.sh
+
 echo "Starting sed_utils test..."
-sed_utils_test.sh
+./sed_utils_test.sh
 
 echo "Starting validation_utils test"
-validator_utils_test.sh
+./validator_utils_test.sh
